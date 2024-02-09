66f4125294cb118a027cbbfc58e2140b681faf41
tato <santiago.aillon.prada@hva.nl>
2024-02-09 10:51:43 -0500

    CONVEX HULL

--- GEOMETRIA/CORTE 1/convexHull.py
+++ GEOMETRIA/CORTE 1/convexHull.py
@@ -1,10 +1,12 @@
 import matplotlib.pyplot as plt
 import numpy as np
+import random
 
-class Vector:
-  def __init__(self, inicial , final):
-    self.inicial = inicial
-    self.final = final
+
+""" 
+CLASSES
+""" 
+
 
 class Punto:
     def __init__(self, x , y):
@@ -17,30 +19,26 @@ class Punto:
     def __str__(self):
         return f"x:{self.x} y:{self.y}"
     
-def plot_vecs(cH): 
-    for i in range (len(cH)):
-        plt.plot(cH[i].x, cH[i].y, 'bo', linestyle="--")
-
+    def __lt__(self, other):
+        if (self.x < other.x):
+            return self.x < other.x
+        elif (self.x == other.x):
+            return self.y < other.y
     
-def leftmost(points):
-    minim = 0
-    for i in range(1,len(points)):
-        if points[i][0] < points[minim][0]:
-            minim = i
-        elif points[i][0] == points[minim][0]:
-            if points[i][1] > points[minim][1]:
-                minim = i
-    return minim
+    def __gt__(self, other):
+        if (self.x > other.x):
+            return self.x > other.x
+        elif(self.x == other.x):
+            return self.y > other.y
 
-def det(p1, p2, p3):
-    """ 
-    > 0: CCW turn
-    < 0 CW turn
-    = 0: colinear
-    """
-    return (p2[0] - p1[0]) * (p3[1] - p1[1]) \
-        -(p2[1] - p1[1]) * (p3[0] - p1[0])
-
+""" 
+CLASSES
+""" 
+ 
+""" 
+FUNCTIONS
+""" 
+    
 def rotation(punto1, punto2):
     ans = punto1.x*punto2.y - punto1.y*punto2.x
     if ans > 0:
@@ -53,55 +51,102 @@ def turn(punto1, punto2, punto3):
     v1 = Punto(punto2.x - punto1.x, punto2.y - punto1.y)
     v2 = Punto(punto3.x - punto1.x, punto3.y - punto1.y)
     rot = rotation(v1, v2)
-    
-    print(rot)
     return rot
 
-points = [(2,1),(1,2),(2,2),(5,4),(3,4),(1,2)]
 
-hull = []
-l = leftmost(points)
-leftMost = points[l]
-currentVertex = leftMost
-hull.append(currentVertex)
-nextVertex = points[1]
-index = 2
-nextIndex = -1
-while True:
-    c0 = currentVertex
-    c1 = nextVertex
-
-    checking = points[index]
-    c2 = checking
-
-    crossProduct = det(currentVertex, nextVertex, checking)
-    if crossProduct < 0:
-        nextVertex = checking
-        nextIndex = index
-    index += 1
-    if index == len(points):
-        if nextVertex == leftMost:
-            break
-        index = 0
-        hull.append(nextVertex)
-        currentVertex = nextVertex
-        nextVertex = leftMost
-print(hull)
-
-def create_points(ct, min = 0, max = 50):
-    return [[randint(min, max), randint(min, max)]\
-        for _ in range(ct)] 
-
-def scatter_plot(coords, convex_hull = None):
-    xs, ys = zip(*coords) #unzip into x and y coordinates
-    plt.scatter(xs, ys)
-
-    if convex_hull:
-        for i in range(1, len(convex_hull) + 1):
-            if i == len(convex_hull): i = 0 #wrap
-            c0 = convex_hull[i-1]
-            c1 = convex_hull[i]
-            plt.plot((c0[0], c1[0]), (c0[1], c1[1]), 'r')
-    plt.show()
+def plotPoints(points, hull):
+    dataL = []
+    fig, (ax1, ax2) = plt.subplots(2)
     
-scatter_plot(points, hull)
+    #PLOT 1
+    for i in points:
+        dataL.append([i.x, i.y])
+    
+    data = np.array(dataL)  
+    x, y = data.T
+    ax1.scatter(x,y)
+    
+    ax1.set_xlim(left=0, right=12)
+    ax1.set_ylim(bottom=0, top=12)
+    
+    #PLOT 2
+    ax2.scatter(x,y)
+    x_values = []
+    y_values = []
+    
+    for i in hull:
+        x_values.append(i.x)
+        y_values.append(i.y)
+        
+    x_values.append((hull[0]).x)
+    y_values.append((hull[0]).y)    
+    
+    ax2.plot(x_values, y_values, 'bo', linestyle="--")
+    
+    ax2.set_xlim(left=0, right=12)
+    ax2.set_ylim(bottom=0, top=12)
+
+def convexHull(points):
+    hull = []  
+    points.sort()
+    print(points)
+    lUpper = []
+    lLower = []
+    n = len(points)
+    
+    #PUT THE POINTS P1 & P2 IN LUPPER
+    lUpper.append(points[0])
+    lUpper.append(points[1])
+    
+    for i in range (2, n):
+        lUpper.append(points[i])
+        #LUPPER CONTAINS MORE THAN 2 AND LAST 3 DONT MAKE A RIGHT TURN
+        while(len(lUpper) > 2 and turn(lUpper[-3:][0],lUpper[-3:][1],lUpper[-3:][2]) != 1):
+            lUpper.remove(lUpper[-3:][1])
+    
+    #PUT THE POINTS Pn & Pn-1 IN Llower
+    lLower.append(points[-2:][1])
+    lLower.append(points[-2:][0])
+    
+    for i in range (n-1, 0, -1):
+        lLower.append(points[i])
+        
+        #LLOWER CONTAINS MORE THAN 2 AND LAST 3 DONT MAKE A RIGHT TURN
+        while(len(lLower) > 2 and turn(lLower[-3:][0],lLower[-3:][1],lLower[-3:][2]) != 1):
+            lLower.remove(lLower[-3:][1])
+    
+    #REMOVE THE FIRST AND LAST OF LLOWER TO AVOID DUPLICATION
+    lLower.remove(lLower[0])
+    lLower.remove(lLower[-1])
+    
+    print("UPPER: " + str(lUpper))
+    print("LOWER: " + str(lLower))
+    
+    hull = lUpper + lLower
+    
+    return hull
+
+
+def generateRandomPoints(n):
+    points = []
+    
+    for i in range (0, n):
+        x = random.randint(0,10)
+        y = random.randint(0,10)
+        points.append(Punto(x,y))
+        
+    return points
+
+""" 
+FUNCTIONS
+"""
+
+P = generateRandomPoints(5)
+print(P)
+hull = convexHull(P)
+
+print("HULL: " + str(hull))
+plotPoints(P, hull)
+
+
+plt.show()
