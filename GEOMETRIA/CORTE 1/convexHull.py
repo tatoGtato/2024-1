3601fa660c7a631e421dc94ac2748f2fe30036bd
tato <santiago.aillon.prada@hva.nl>
2024-02-26 21:06:31 -0500

    ashjashj

--- GEOMETRIA/CORTE 1/convexHull.py
+++ GEOMETRIA/CORTE 1/convexHull.py
@@ -1,6 +1,7 @@
 import matplotlib.pyplot as plt
 import numpy as np
 import random
+import time
 
 
 """ 
@@ -14,10 +15,10 @@ class Punto:
         self.y = y
         
     def __repr__(self):
-        return f"x:{self.x} y:{self.y}"
+        return f"(x:{self.x},y:{self.y})"
         
     def __str__(self):
-        return f"x:{self.x} y:{self.y}"
+        return f"(x:{self.x},y:{self.y})"
     
     def __lt__(self, other):
         if (self.x < other.x):
@@ -52,39 +53,6 @@ def turn(punto1, punto2, punto3):
     v2 = Punto(punto3.x - punto1.x, punto3.y - punto1.y)
     rot = rotation(v1, v2)
     return rot
-
-
-def plotPoints(points, hull):
-    dataL = []
-    fig, (ax1, ax2) = plt.subplots(2)
-    
-    #PLOT 1
-    for i in points:
-        dataL.append([i.x, i.y])
-    
-    data = np.array(dataL)  
-    x, y = data.T
-    ax1.scatter(x,y)
-    
-    ax1.set_xlim(left=-1, right=12)
-    ax1.set_ylim(bottom=-1, top=12)
-    
-    #PLOT 2
-    ax2.scatter(x,y)
-    x_values = []
-    y_values = []
-    
-    for i in hull:
-        x_values.append(i.x)
-        y_values.append(i.y)
-        
-    x_values.append((hull[0]).x)
-    y_values.append((hull[0]).y)    
-    
-    ax2.plot(x_values, y_values, 'bo', linestyle="--")
-    
-    ax2.set_xlim(left=-1, right=12)
-    ax2.set_ylim(bottom=-1, top=12)
 
 def convexHull(points):
     hull = []  
@@ -126,6 +94,37 @@ def convexHull(points):
     
     return hull
 
+def plotPoints(points, hull):
+    dataL = []
+    fig, (ax1, ax2) = plt.subplots(2)
+    
+    #PLOT 1
+    for i in points:
+        dataL.append([i.x, i.y])
+    
+    data = np.array(dataL)  
+    x, y = data.T
+    ax1.scatter(x,y)
+    
+    ax1.set_xlim(left=-1, right=102)
+    ax1.set_ylim(bottom=-1, top=102)
+    
+    #PLOT 2
+    ax2.scatter(x,y)
+    x_values = []
+    y_values = []
+    
+    for i in hull:
+        x_values.append(i.x)
+        y_values.append(i.y)
+        
+    x_values.append((hull[0]).x)
+    y_values.append((hull[0]).y)    
+    
+    ax2.plot(x_values, y_values, 'bo', linestyle="--")
+    
+    ax2.set_xlim(left=-1, right=102)
+    ax2.set_ylim(bottom=-1, top=102)
 
 def generateRandomPoints(n):
     points = []
@@ -141,12 +140,49 @@ def generateRandomPoints(n):
 FUNCTIONS
 """
 
-P = generateRandomPoints(9)
-print(P)
-hull = convexHull(P)
+"""
+TESTS
+"""
 
-print("HULL: " + str(hull))
-plotPoints(P, hull)
+def generate_test_case():
+    """Genera un caso de prueba con un conjunto de 9 puntos aleatorios."""
+    points = [Punto(random.randint(0, 100), random.randint(0, 100)) for _ in range(9)]
+    return points
+
+def run_test_case(points):
+    """Ejecuta el algoritmo convexHull en un conjunto de puntos y mide el tiempo."""
+    start_time = time.time()
+    convex_hull = convexHull(points)
+    end_time = time.time()
+    execution_time = end_time - start_time
+    return convex_hull, execution_time
+
+def print_test_case_result(test_case_number, points, convex_hull, execution_time):
+    """Imprime el resultado de un caso de prueba."""
+    print(f"Test Case {test_case_number}:")
+    print("Input:")
+    for point in points:
+        print(point)
+    print("Output (Convex Hull):")
+    for point in convex_hull:
+        print(point)
+    print(f"Execution Time: {execution_time} seconds")
+    print()
+
+# Ejecutar las pruebas
+for i in range(6):
+    points = generate_test_case()
+    convex_hull, execution_time = run_test_case(points)
+    print_test_case_result(i+1, points, convex_hull, execution_time)
+    plotPoints(points, convex_hull)
+
+
+# P = generateRandomPoints(9)
+# print(P)
+# hull = convexHull(P)
+
+# print("HULL: " + str(hull))
+
 
 
 plt.show()
