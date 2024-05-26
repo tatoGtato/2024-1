c0eb156ed570284ad368407830355b8db646e9bb
tato <santiago.aillon.prada@hva.nl>
2024-05-26 18:02:48 -0500

    senales

--- MASD2/Punto3_Parcial3.m
+++ MASD2/Punto3_Parcial3.m
@@ -6,18 +6,18 @@ for m=0:2
     figure(1)
     plot(x1,Jm);
     hold on;
-    Km=besselk(m,x2);
-    figure(2)
-    plot(x2,Km);
-    hold on;
+    % Km=besselk(m,x2);
+    % figure(2)
+    % plot(x2,Km);
+    % hold on;
 end
-figure(1);
-xlabel('x');
-ylabel('J_m(x)');
-legend('m=0','m=1','m=2','m=3','m=4','m=5');
-hold off;
-figure(2);
-xlabel('x');
-ylabel('K_m(x)');
-legend('m=0','m=1','m=2','k=3','m=4','m=5');
-hold off;
+% figure(1);
+% xlabel('x');
+% ylabel('J_m(x)');
+% legend('m=0','m=1','m=2','m=3','m=4','m=5');
+% hold off;
+% figure(2);
+% xlabel('x');
+% ylabel('K_m(x)');
+% legend('m=0','m=1','m=2','k=3','m=4','m=5');
+% hold off;
